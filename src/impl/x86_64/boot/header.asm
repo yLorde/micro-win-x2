46d733f51b0e747a617e883a4e65a8566f9f6b38
Davi de Sousa <davisouza2204@gmail.com>
2025-12-24 07:43:04 -0300

    Adicionando C

--- src/impl/x86_64/boot/header.asm
+++ src/impl/x86_64/boot/header.asm
@@ -3,10 +3,11 @@ header_start:
 	; magic number
 	dd 0xe85250d6 ; multiboot2
 	; architecture
-	dd 0 ; protected mode i386
+	dd 0
+	; protected mode i386
 	; header length
 	dd header_end - header_start
-	; checksum
+	;  checksum 
 	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
 	; end tag
